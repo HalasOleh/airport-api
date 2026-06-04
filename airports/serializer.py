aa80f3f4c60238dece75c4f725d5386d02cad003
Oleh Halas <oleggalas8@gmail.com>
2026-06-04 18:55:47 +0300

    Implement CRUD, update models, update admin, add logging

--- airports/serializer.py
+++ airports/serializer.py
@@ -1,9 +1,69 @@
 from rest_framework import serializers
-from airports.models import Ticket
+from airports.models import Ticket, Country, Airport, Airline, Airplane, Flight
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ("id", "name", "visa_required")
+        read_only_fields = ("id",)
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "city", "code", "country")
+        read_only_fields = ("id",)
+
+
+class AirlineSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Airline
+        fields = ("id", "name", "founded_year", "headquarters", "airport")
+        read_only_fields = ("id",)
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = ("id", "model", "num_seats", "airline")
+        read_only_fields = ("id",)
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    departure = serializers.DateTimeField(
+        input_formats=["%d/%m/%Y"]
+    )
+    arrival = serializers.DateTimeField(
+        input_formats=["%d/%m/%Y"]
+    )
+
+    class Meta:
+        model = Flight
+        fields = ("id", "status", "trip", "departure", "arrival", "airplane")
+        read_only_fields = ("id",)
+
+    def validate(self, data):
+        departure = data.get("departure")
+        arrival = data.get("arrival")
+
+        if arrival <= departure:
+            logger.warning(
+                f"Invalid flight dates. "
+                f"Departure={departure}, Arrival={arrival}"
+            )
+            raise serializers.ValidationError("Date entered incorrectly")
+
+        return data
+
 
 class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ticket
-        fields = ("id", "status", "seat", "trip",)
-        read_only_fields = ("id",)
+        fields = ("id", "status", "seat", "flight", "user")
+        read_only_fields = ("id", "status")
--- airports/urls.py
+++ airports/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, include
-from airports.views import TicketViewSet
+from airports.views import CountryViewSet, AirportViewSet, AirlineViewSet, AirplaneViewSet, FlightViewSet, TicketViewSet
 from rest_framework import routers
 
 app_name = 'airports'
 
 router = routers.DefaultRouter()
 
+
+router.register('country', CountryViewSet)
+router.register('airport', AirportViewSet)
+router.register('airline', AirlineViewSet)
+router.register('airplane', AirplaneViewSet)
+router.register('flight', FlightViewSet)
 router.register('ticket', TicketViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
 ]
