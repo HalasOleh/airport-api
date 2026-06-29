faeb44043da0823cdc60360811f01d554c5684ac
Oleh Halas <oleggalas8@gmail.com>
2026-06-29 10:45:20 +0300

    Add 'role' in UserManager

--- user/models.py
+++ user/models.py
@@ -28,6 +28,7 @@ class UserManager(DjangoUserManager):
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', User.Roles.ADMIN)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
